d980855ad535d8f8cd9c5a825e0877b6f154d29a
Trasca Sebastian <sebastiangeoioge@gmail.com>
2024-04-09 19:22:04 +0300

    added the functionality to display contact details

--- src/main/java/guiapptest/contactapp/ContactListController.java
+++ src/main/java/guiapptest/contactapp/ContactListController.java
@@ -1,9 +1,13 @@
 package guiapptest.contactapp;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.util.Callback;
 
 import java.net.URL;
@@ -12,6 +16,18 @@ import java.util.ResourceBundle;
 public class ContactListController implements Initializable {
     @FXML
     private ListView<Contact> contactListView;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField phoneTextField;
+
+    @FXML
+    private TextField emailTextField;
+
+    private Contact currentContact;
+
     Contact c1 = new Contact("Tudor", "292839","tudi23@gmail.com");
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -22,5 +38,15 @@ public class ContactListController implements Initializable {
                 return new ContactCellFactory();
             }
         });
+        contactListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Contact>() {
+            @Override
+            public void changed(ObservableValue<? extends Contact> observableValue, Contact contact, Contact t1) {
+                currentContact = contactListView.getSelectionModel().getSelectedItem();
+                nameTextField.setText(currentContact.getName());
+                phoneTextField.setText(currentContact.getPhoneNumber());
+                emailTextField.setText(currentContact.getEmail());
+            }
+        });
     }
+
 }
