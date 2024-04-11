63f93df80cbf8968e193e31567ce40b12428b5af
Trasca Sebastian <sebastiangeoioge@gmail.com>
2024-04-11 18:06:50 +0300

    added an icon to the app

--- src/main/java/guiapptest/contactapp/App.java
+++ src/main/java/guiapptest/contactapp/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class App extends Application {
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
         stage.setTitle("Contact Storage");
+        stage.getIcons().add(new Image(App.class.getResourceAsStream("/contactlogo.png")));
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
